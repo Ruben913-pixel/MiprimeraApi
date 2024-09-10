5c97c74f3967c486fd4fe0b60a89c5477ea41d54
Ruben913-pixel <rubencastellanos913@gmail.com>
2024-09-10 18:15:59 +0200

    agrege los archivos de mi primera api a git

---
+++ index.js
@@ -0,0 +1,75 @@
+const express =  require('express')
+const cors= require("cors")
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+  ]
+
+
+app.get('/', (requise,response) => {
+    response.send('<h1>hola con express</h1>')
+})
+app.get('/api/notes', (requise,response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (r,response) => {
+    const id = Number(r.params.id) 
+    console.log({id})
+    const note = notes.find(note => note.id === parseInt(id))
+    if(note){
+    response.json(note)
+    }else {
+      response.status(404).end()
+    }
+
+})
+
+app.delete('/api/notes/:id', (r,response) => {
+    const id = Number(r.params.id) 
+    console.log({id})
+
+    const note = notes.filter(note => note.id !== parseInt(id))
+    console.log({note})
+    response.json(note)
+})
+
+
+app.post('/api/notes', (r, response) => {
+  const {content} = r.body
+if(!content){
+  console.log("error 404")
+  return response.status(404).end()
+}
+  const newNote = {
+    id: notes.length + 1 ,
+    content: content,
+    important: false
+  }
+  notes = [...notes,newNote]
+  response.json(newNote)
+})
+
+const PORT = 3002
+app.listen(PORT, ()=> {
+    console.log(`Server running on port ${PORT}`)
+})
